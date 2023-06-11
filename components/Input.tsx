462665a18880a49cea8bd834bf4d6beef1a8d3de
Aaron <aarongrossman.dev@gmail.com>
2023-06-11 17:29:27 -0400

    fixed ref, and typescript error on Input component

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,9 +1,10 @@
 import { forwardRef } from "react";
 import { twMerge } from "tailwind-merge";
 
-interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {}
 
-const Input = forwardRef<InputProps, HTMLInputElement>(
+const Input = forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, disabled, ...props }, ref) => {
     return (
       <input
@@ -12,6 +13,7 @@ const Input = forwardRef<InputProps, HTMLInputElement>(
           `flex w-full rounded-md bg-neutral-700 border border-transparent px-3 py-3 text-sm file:border-0 
           file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-400 
           disabled:cursor-not-allowed disabled:opacity-50 focus:outline-none`,
+          disabled && "opacity-75",
           className
         )}
         disabled={disabled}
